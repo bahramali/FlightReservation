4aa8777509d0eea6308c31aa8232c2454cbb8944
Administrator <Administrator@DESK-FDC7M82.lexicon.edu.local>
2017-07-12 13:37:06 +0200

    Add food

---
+++ FlightReservation/src/main/java/models/FoodService.java
@@ -0,0 +1,45 @@
+package models;
+
+public class FoodService {
+	
+	private String menuName;
+	private String foodItem;
+	private double price;
+	
+	
+	public FoodService(String menuName, String foodItem, double price) {
+		super();
+		this.menuName = menuName;
+		this.foodItem = foodItem;
+		this.price = price;
+	}
+
+
+	
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+
+	public String getFoodItem(String foodItem) {
+		return foodItem;
+	}
+	
+	public void setFoodItem(String foodItem) {
+		this.menuName = foodItem;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+
+}
