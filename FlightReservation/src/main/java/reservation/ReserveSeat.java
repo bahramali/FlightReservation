98c89b7902c39208a7ce7212ad399101b98d92c3
User <User@DESK-DWJPBB2.lexicon.edu.local>
2017-07-13 13:03:56 +0200

    add package definition

--- FlightReservation/src/main/java/reservation/ReserveSeat.java
+++ FlightReservation/src/main/java/reservation/ReserveSeat.java
@@ -1,30 +1,31 @@
+package reservation;
 
 public class ReserveSeat {
-private Flight theFlight;
-private boolean isItFirstClass;
-private int seat;
-private int cost;
+	private Flight theFlight;
+	private boolean isItFirstClass;
+	private int seat;
+	private int cost;
 
-public ReserveSeat(Flight flight,boolean isItFirstClass) {
-	this.isItFirstClass=isItFirstClass;
-	this.theFlight=flight;
-	if (this.isItFirstClass) {
-		this.seat=this.theFlight.reserveAFirstClassSeat();
-		if (this.seat==0) {this.cost=0;}
-		else {this.cost=20000;}
-	}else {
-		this.seat=this.theFlight.reserveAnEconomyClassSeat();
-		if (this.seat==0) {this.cost=0;}
-		else {this.cost=5000;}		
+	public ReserveSeat(Flight flight,boolean isItFirstClass) {
+		this.isItFirstClass=isItFirstClass;
+		this.theFlight=flight;
+		if (this.isItFirstClass) {
+			this.seat=this.theFlight.reserveAFirstClassSeat();
+			if (this.seat==0) {this.cost=0;}
+			else {this.cost=20000;}
+		}else {
+			this.seat=this.theFlight.reserveAnEconomyClassSeat();
+			if (this.seat==0) {this.cost=0;}
+			else {this.cost=5000;}		
+		}
 	}
-}
 
-public int getSeat() {
-	return seat;
-}
+	public int getSeat() {
+		return seat;
+	}
 
-public int getCost() {
-	return cost;
-}
+	public int getCost() {
+		return cost;
+	}
 
 }
