f03f586f0be796619e2736a2ce86e33a58c693d1
User <User@DESK-DWJPBB2.lexicon.edu.local>
2017-07-13 13:05:33 +0200

    add empty unimplament method

--- FlightReservation/src/main/java/reservation/Reservation.java
+++ FlightReservation/src/main/java/reservation/Reservation.java
@@ -1,5 +1,13 @@
 package reservation;
 
-public class Reservation {
+import java.util.List;
+
+public class Reservation implements ReservationInterface{
+
+	@Override
+	public List<Reservation> getAllReservations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
