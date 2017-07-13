7e4e40e885c64432a0aa4113a3924c98a1ff78ab
User <User@DESK-DWJPBB2.lexicon.edu.local>
2017-07-13 11:03:20 +0200

    Interface including constructor-interface for the Seat-reservation

--- FlightReservation/src/main/java/reservation/ReservationInterface.java
+++ FlightReservation/src/main/java/reservation/ReservationInterface.java
@@ -3,6 +3,7 @@ package reservation;
 public interface ReservationInterface {
 
 	public abstract ReserveSeat(Flight flight,boolean isItFirstClass);
+	
 	public abstract int getSeat() ;
 	public abstract int getCost() ;
 }
