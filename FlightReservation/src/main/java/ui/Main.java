9a52e8b261a1959a719540083b8cc9dc7d4c59f9
User <User@DESK-DWJPBB2.lexicon.edu.local>
2017-07-13 13:08:25 +0200

    rengöra main method

--- FlightReservation/src/main/java/ui/Main.java
+++ FlightReservation/src/main/java/ui/Main.java
@@ -1,12 +1,10 @@
 package ui;
 
-import models.Customer;
 
 public class Main {
 	
 	public static void main(String[] args) {
 
-		Customer customer = new Customer("caroline ", " br12345");
 		
 	}
 }
