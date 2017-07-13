9ada5b26ddfff4ba62dd857ba88d0f249a6a95f6
User <User@DESK-DWJPBB2.lexicon.edu.local>
2017-07-13 11:24:02 +0200

    Airplane Class: Code compiles, but it i have not tested if it runs properly, and i have not comented the code.

--- FlightReservation/src/main/java/reservation/Airplane.java
+++ FlightReservation/src/main/java/reservation/Airplane.java
@@ -1,8 +1,5 @@
 package reservation;
 
-
-import java.util.LinkedList;
-
 public class Airplane {
 	private int MaxNumberOfEconomyClassSeats;
 	private int MaxNumberOfFirstClassSeats;
@@ -18,18 +15,6 @@ public class Airplane {
 		super();
 		this.MaxNumberOfEconomyClassSeats = 5;
 		this.MaxNumberOfFirstClassSeats = 5;
-		int count=1;
-		this.listOfFreeEconomyClassSeats = new LinkedList<Integer>();
-		this.listOfFreeFirstClassSeats = new LinkedList<Integer>();
-
-		while (count<this.MaxNumberOfEconomyClassSeats) {
-			this.listOfFreeEconomyClassSeats.add(count++);
-
-		}
-		int sum =  this.MaxNumberOfEconomyClassSeats+this.MaxNumberOfFirstClassSeats;
-		while (count<sum) {
-			this.listOfFreeFirstClassSeats.add(count++);	
-		}
 	}
 
 	public int getMaxNumberOfEconomyClassSeats() {
