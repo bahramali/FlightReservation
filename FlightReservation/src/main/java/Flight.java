d56f2afdd40f653172bee84d4d5bc0ab26e1c7c2
User <User@DESK-DWJPBB2.lexicon.edu.local>
2017-07-13 12:23:58 +0200

    Flight and ReserveSeat have the functions and they compile. Not commented and not run-tested.

---
+++ FlightReservation/src/main/java/Flight.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+
+import reservation.Airplane;
+
+public class Flight {
+	private LinkedList<Integer> listOfFreeEconomyClassSeats;
+	private LinkedList<Integer> listOfFreeFirstClassSeats;
+	private Airplane flyingMachine;
+	private int MaxNumberOfEconomyClassSeats;
+	private int MaxNumberOfFirstClassSeats;
+	
+	public Flight(Airplane flyingMachine) {
+		super();
+		this.flyingMachine = flyingMachine;
+		this.MaxNumberOfEconomyClassSeats=this.flyingMachine.getMaxNumberOfEconomyClassSeats();
+		this.MaxNumberOfFirstClassSeats=this.flyingMachine.getMaxNumberOfFirstClassSeats();
+		
+		int count=1;
+		while (count<MaxNumberOfFirstClassSeats) {
+		this.listOfFreeFirstClassSeats.add(count++);
+		}
+		
+		int sum =  this.MaxNumberOfEconomyClassSeats+this.MaxNumberOfFirstClassSeats;
+		while (count<sum) {
+			this.listOfFreeEconomyClassSeats.add(count++);		
+		}
+	}
+
+	public int reserveAnEconomyClassSeat() {
+	if (listOfFreeEconomyClassSeats.isEmpty()) {
+		System.out.println("No free economy class seats available");
+		return 0;}
+	else return listOfFreeEconomyClassSeats.pollFirst();
+	}
+
+	public int reserveAFirstClassSeat() {
+	if (listOfFreeFirstClassSeats.isEmpty()) {
+		System.out.println("No free first class seats available");
+		return 0;}
+	else return listOfFreeFirstClassSeats.pollFirst();
+	}
+
+}
---
+++ FlightReservation/src/main/java/ReserveSeat.java
@@ -0,0 +1,30 @@
+
+public class ReserveSeat {
+private Flight theFlight;
+private boolean isItFirstClass;
+private int seat;
+private int cost;
+
+public ReserveSeat(Flight flight,boolean isItFirstClass) {
+	this.isItFirstClass=isItFirstClass;
+	this.theFlight=flight;
+	if (this.isItFirstClass) {
+		this.seat=this.theFlight.reserveAFirstClassSeat();
+		if (this.seat==0) {this.cost=0;}
+		else {this.cost=20000;}
+	}else {
+		this.seat=this.theFlight.reserveAnEconomyClassSeat();
+		if (this.seat==0) {this.cost=0;}
+		else {this.cost=5000;}		
+	}
+}
+
+public int getSeat() {
+	return seat;
+}
+
+public int getCost() {
+	return cost;
+}
+
+}
