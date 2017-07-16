86cc3646a05da3a894f8df424a412f65b35949ce
Azad <Azad@10.0.1.20>
2017-07-16 12:02:56 +0200

    split departre_dateTime to localDate and localTime.

--- FlightReservation/src/main/java/reservation/Flight.java
+++ FlightReservation/src/main/java/reservation/Flight.java
@@ -1,20 +1,31 @@
 package reservation;
 
-import java.time.LocalDateTime;
-
-import org.omg.PortableInterceptor.DISCARDING;
+import java.time.*;
 
 public class Flight
 {
+	private String flightNumber;
 	private City origin;
 	private City destination;
-	private LocalDateTime departured_Datetime;
+	private LocalDate departure_Date;
+	private LocalTime departure_Time;
 	
-	
-	public Flight(City origin, City destination,LocalDateTime departured_Datetime) {
+	public Flight(String flightNumber,City origin, City destination,LocalDate departure_Date,LocalTime departureTime) {
+		this.flightNumber = flightNumber;
 		this.origin = origin;
 		this.destination = destination;
-		this.departured_Datetime = departured_Datetime;
+		this.departure_Date = departure_Date;
+		this.departure_Time = departureTime;
+	}
+	
+	public String getFlightNumber()
+	{
+		return flightNumber;
+	}
+
+	public void setFlightNumber(String flightNumber)
+	{
+		this.flightNumber = flightNumber;
 	}
 
 	public City getOrigin()
@@ -37,20 +48,29 @@ public class Flight
 		this.destination = destination;
 	}
 
-	public LocalDateTime getDepartured_Datetime()
+	public LocalDate getDeparture_Date()
 	{
-		return departured_Datetime;
+		return departure_Date;
 	}
 
-	public void setDepartured_Datetime(LocalDateTime departured_Datetime)
+	public void setDeparture_Date(LocalDate departure_Date)
 	{
-		this.departured_Datetime = departured_Datetime;
+		this.departure_Date = departure_Date;
 	}
 
+	public LocalTime getDeparture_Time()
+	{
+		return departure_Time;
+	}
+
+	public void setDeparture_Time(LocalTime departure_Time)
+	{
+		this.departure_Time = departure_Time;
+	}
 
 	@Override
 	public String toString()
 	{
-		return String.format("from %-8s to %-8s in %-8s ", origin,destination,departured_Datetime);
+		return String.format("%-6s from %-8sto %-8s in %-8s  %-6s ",flightNumber, origin,destination,departure_Date.toString(),departure_Time);
 	}
 }
